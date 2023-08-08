197a491161f9f7a21de2cf3bfcd006d9f0b17a59
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-08-08 21:12:59 +0530

    Complete 3

---
+++ Demo3.java
@@ -0,0 +1,58 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int y = 0;
+        while (y++ < 4) {
+            int x = 0;
+            while(x++ < 7) {
+                if (y == 2 && x == 4) {
+                    int z = 0;
+                    while (z++ < 1) {
+                        System.out.print("  ");
+                    }
+                    x += 1;
+                }
+
+                if (y == 3 && x == 3) {
+                    int z = 0;
+                    while (z++ < 3) {
+                        System.out.print("  ");
+                    }
+                    x += 3;
+                }
+
+                if (y == 4 && x == 2) {
+                    int z = 0;
+                    while (z++ < 5) {
+                        System.out.print("  ");
+                    }
+                    x += 5;
+                }
+                
+                System.out.print("+ ");
+            }
+            System.out.println();
+        }
+        while (y++ < 8){
+            int x = 0;
+            while (x++ < 7) {
+                if (y - 1 == 5 && x == 3) {
+                    int z = 0;
+                    while (z++ < 3) {
+                        System.out.print("  ");
+                    }
+                    x += 3;
+                }
+                
+                if (y - 1 == 6 && x == 4) {
+                    int z = 0;
+                    while (z++ < 1) {
+                        System.out.print("  ");
+                    }
+                    x += 1;
+                }
+                System.out.print("+ ");
+            }
+            System.out.println();
+        }
+    }
+}
